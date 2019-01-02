04927f836c0c7b5989362fa9d3f39586708d672b
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-02 23:50:02 +0300

    Create gif.js

---
+++ commands/gif.js
@@ -0,0 +1,27 @@
+const puppy = require('random-puppy')
+const Discord = require("discord.js");
+ 
+ 
+module.exports.run = (bot, message, args, discord) => {
+  if (!message.channel.nsfw) return message.channel.send(":underage: You need to be in an NSFW channel to use this command.")
+  let keywords = [
+    "porngif",
+    "sexgif",
+    "gifporn",
+    "gifsex",
+    "porn_gif",
+    "sex_gif"
+  ]
+  
+  var result = keywords[Math.floor(Math.random()*keywords.length)]
+  
+  puppy(result).then(url => {
+    let embed = new Discord.RichEmbed()
+    .setTitle("Ass")
+    .setDescription("Here's an ass pic...")
+    .setImage(url)
+    .setTimestamp()
+    .setFooter(`Requested by ${message.author.username}`)
+    message.channel.send({embed: embed})
+  })
+}
