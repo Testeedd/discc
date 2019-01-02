555bf8ef0d1f3b012b670cef0b55770497e9a46d
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-02 22:00:56 +0300

    Create hentai.js

---
+++ commands/hentai.js
@@ -0,0 +1,22 @@
+const randomPuppy = require('random-puppy');
+const request = require('snekfetch');
+const fs = require("fs")
+
+exports.run = (client, message, args) => {
+    if (!message.channel.nsfw) return message.channel.send(":underage: NSFW Command. Please switch to NSFW channel in order to use this command.")
+
+    var subreddits = [
+        'HENTAI_GIF',
+        'hentai_irl'
+    ]
+    var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+
+    randomPuppy(sub)
+        .then(url => {
+            request.get(url).then(r => {
+                fs.writeFile(`hentai.jpg`, r.body)
+                message.channel.sendFile(r.body)
+                fs.unlink(`./hentai.jpg`)
+            })
+        })
+}
