87872cb890911ea6b24e56415db952efd1488ef4
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-02 21:34:36 +0300

    Add files via upload

---
+++ commands/adminhelp.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+exports.run = (client, message, args, level) => {
+
+const embed = new Discord.RichEmbed()
+  .setTitle("Помощь!")
+  .setColor("#4169E1 ")
+  .setDescription(`
+__ban__ - забанить участника(шban @user 1m/1d/1h)
+__clean__ - очистка чата (шclean <кол-во сообщений>)
+__kick__ - кикнуть участника (шkick @user <причина>)
+__mute__ -мутить участника ( Шmute <время в милисекундах >)
+__news__ - сказать новость ( Шnews <новость>) 
+**[ПРЕФИКС НА ДАННЫЙ МОМЕНТ = Ш]**`)
+  .setTimestamp()
+  .setFooter("|LLIAJIYH-BOT| PROJECTS All rightes reserved.", "https://i.imgur.com/fBv7xWF.jpg")
+
+  message.author.send({embed})
+message.delete().catch(_O_o=>{});
+	} 
