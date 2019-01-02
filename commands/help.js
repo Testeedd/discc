21019a84397f3a398424f814afa1daaafd00099c
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-02 15:48:28 +0300

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+exports.run = (client, message, args, level) => {
+
+const embed = new Discord.RichEmbed()
+  .setTitle("Помощь!")
+  .setColor("#4169E1 ")
+  .addField("__Help__", `Данное меню`)
+  .addField("__Dev__", `создатель бота`)
+  .addField("__cmds__", `все команды`)
+  .addField("__invite__", `пригласить бота`)
+  .setTimestamp()
+  .setFooter("|LLIAJIYH-BOT| PROJECTS All rightes reserved.", "https://i.imgur.com/fBv7xWF.jpg")
+message.channel.send({embed}).then(msg => msg.delete(10000));
+message.delete().catch(_O_o=>{});
+	} 
