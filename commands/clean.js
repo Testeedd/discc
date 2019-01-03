6a0cc20e85044aa840de10ce58bb144cf30774e3
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-03 13:45:17 +0300

    Update clean.js

--- commands/clean.js
+++ commands/clean.js
@@ -1,11 +1,9 @@
-const Discord = require("discord.js");
-const errors = require("../utils/errors.js");
+exports.run = (client, message, args, tools) => {
+	
+if (isNaN(args[0])) return message.channel.send('**укажите кол-во сообщений**');
+if (args[0] > 100) return message.channel.send('**кол-во сообщений должно быть меньше 100**')
 
-module.exports.run = async (bot, message, args) => {
-
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
-  if(!args[0]) return message.channel.send("oof");
-  message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
-  });
+  message.channel.bulkDelete(args[0])
+    .then( message => message.channel.send(`**успешно удалено \`${message.size}/${args[0]}\` сообщений**`).then( msg => msg.delete({timeout: 10000})))
+	.catch( error => message.channel.send(`**ERROR** ${error.message}`));
 }
