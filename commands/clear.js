5b331139751102d26e08640c4da22fb491f1f0cb
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-03 12:58:15 +0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,11 +1,12 @@
 const Discord = require("discord.js");
+const moment = require("moment");
 const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => {
-
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
-  if(!args[0]) return message.channel.send("oof");
-  message.channel.bulkDelete(args[0]).then(() => {
+ if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+ if(!args[0]) return message.channel.send("укажите кол-во сообщений");
+ message.channel.bulkDelete(args[0]).then(() => {
     message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
   });
-}
+} 
+
