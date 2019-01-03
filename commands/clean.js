1daea49f1f2981a17cd43f0b3031d8833df4d952
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-03 13:49:21 +0300

    Update clean.js

--- commands/clean.js
+++ commands/clean.js
@@ -7,5 +7,4 @@ if (args[0] > 100) return message.channel.send('**кол-во сообщений
 
   message.channel.bulkDelete(args[0])
     .then( message => message.channel.send(`**успешно удалено \`${message.size}/${args[0]}\` сообщений**`).then( msg => msg.delete({timeout: 10000})))
-	.catch( error => message.channel.send(`**ERROR** ${error.message}`));
 }
