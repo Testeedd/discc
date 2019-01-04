067171cd1eaaa5d56a7b672440db4d5701d0bb62
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-04 19:14:13 +0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,21 +1,24 @@
 const Discord = require('discord.js');
+const settings = require('../config.json');
 exports.run = (client, message, args) => {
   let reason = args.slice(1).join(' ');
   let user = message.mentions.users.first();
-  let modlog = client.channels.find('name', 'mod-log');
-  if (!modlog) return message.reply('I cannot find a mod-log channel');
+  let logchannel = message.guild.channels.find('name', 'logs');
+  if (!logchannel) return message.reply('I cannot find a logs channel');
+  if (!message.member.hasPermission("BAN_MEMBERS")) return msg.reply(":no_entry_sign: **Error:** You don't have the **Ban Members** permission!");
   if (reason.length < 1) return message.reply('You must supply a reason for the ban.');
   if (message.mentions.users.size < 1) return message.reply('You must mention someone to ban them.').catch(console.error);
 
-  if (!message.guild.member(user).bannable) return message.reply('I cannot ban that member');
-  message.guild.ban(user, 2);
+  if (!message.guild.member(user).bannable) return message.reply(`<:redTick:${settings.redTick}> I cannot ban that member`);
+  message.guild.member(user).ban();
 
   const embed = new Discord.RichEmbed()
-    .setColor(0x00AE86)
+    .setColor(0xFF0000)
     .setTimestamp()
     .addField('Action:', 'Ban')
     .addField('User:', `${user.username}#${user.discriminator} (${user.id})`)
-    .addField('Modrator:', `${message.author.username}#${message.author.discriminator}`)
+    .addField('Moderator:', `${message.author.username}#${message.author.discriminator}`)
     .addField('Reason', reason);
-  return client.channels.get(modlog.id).sendEmbed(embed);
+    message.channel.send(`<:hammer:${settings.hammer}> Bippity boppity **BAN**! I\'ve logged the ban in the logs channel.`)
+  return client.channels.get(logchannel.id).send({embed});
 };
