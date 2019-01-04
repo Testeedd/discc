f222857e73e7eab33949664101bd7dd10bad9f6e
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-04 19:14:57 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,29 +1,22 @@
-const Discord = require("discord.js");
-const errors = require("../utils/errors.js");
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+  let reason = args.slice(1).join(' ');
+  let user = message.mentions.users.first();
+  let logchannel = message.guild.channels.find('name', 'logs');
+  if (!logchannel) return message.reply('I cannot find a logs channel');
+  if (reason.length < 1) return message.reply('You must supply a reason for the kick.');
+  if (message.mentions.users.size < 1) return message.reply('You must mention someone to kick them.').catch(console.error);
 
-exports.run = (client, message, args, level) => {
-    if(!message.member.hasPermission("KICK_MEMBERS")) return errors.noPerms(message, "KICK_MEMBERS");
-    if(args[0] == "help"){
-      message.reply("Usage: !kick <user> <reason>");
-      return;
-    }
-    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return errors.cantfindUser(message.channel);
-    let kReason = args.join(" ").slice(22);
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return errors.equalPerms(message, kUser, "MANAGE_MESSAGES");
+  if (!message.guild.member(user).kickable) return message.reply('I cannot kick that member');
+  message.guild.member(user).kick();
 
-    let kickEmbed = new Discord.RichEmbed()
-    .setDescription("~Kick~")
-    .setColor("#e56b00")
-    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
-    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
-    .addField("Kicked In", message.channel)
-    .addField("Tiime", message.createdAt)
-    .addField("Reason", kReason);
-
-    let kickChannel = message.guild.channels.find(`name`, "incidents");
-    if(!kickChannel) return message.channel.send("Can't find incidents channel.");
-
-    message.guild.member(kUser).kick(kReason);
-    kickChannel.send(kickEmbed);
-}
+  const embed = new Discord.RichEmbed()
+    .setColor(0x0000FF)
+    .setTimestamp()
+    .addField('Action:', 'Kick')
+    .addField('User:', `${user.username}#${user.discriminator} (${user.id})`)
+    .addField('Moderator:', `${message.author.username}#${message.author.discriminator}`)
+    .addField('Reason', reason);
+  message.channel.send(':white_check_mark: Success! I\'ve logged the kick in <#293573342999609345>.')
+  return client.channels.get(logchannel.id).send({embed});
+};
