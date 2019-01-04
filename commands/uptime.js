4b2f59b59701799fc2929473bfb040595d09c773
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-04 19:08:00 +0300

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -3,16 +3,17 @@ const botconfig = require('../config.json');
 
 module.exports.run = async (bot, message, args) => {
   let totalSeconds = (bot.uptime / 1000);
+  let days = Math.floor(totalSeconds / 86400)
   let hours = Math.floor(totalSeconds / 3600);
   totalSeconds %= 3600;
   let minutes = Math.floor(totalSeconds / 60);
   let seconds = Math.round(totalSeconds % 60);
 
-  let uptime = `${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+  let uptime = `${days}дней ${hours} часов, ${minutes} минут и ${seconds} секунд`;
 
   let uptimeEmbed = new Discord.RichEmbed()
   .setAuthor(message.author.tag)
-  .setDescription(`My uptime is ${uptime}.`)
+  .setDescription(`Я работаю уже = ${uptime}.`)
   .setColor(botconfig.green);
 
   message.channel.send(uptimeEmbed).then(msg => msg.delete(10000));
