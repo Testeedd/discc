974dd030637f21df8d62cc12481496e706041d43
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-04 17:55:32 +0300

    Update error.js

--- utils/error.js
+++ utils/error.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-let config = require("../botconfig.json");
+let config = require("../config.json");
 
 module.exports.noPerms = (message, perm) => {
     let embed = new Discord.RichEmbed()
