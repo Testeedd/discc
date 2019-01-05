005ea4e5a5a41052b55963f84a145f856feb6a83
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-06 02:12:45 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,6 +2,6 @@ module.exports = (client, member) => {
 
     client.on("guildMemberAdd", (member) => {
   console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
-  member.guild.channels.get("welcome").send(`"${member.user.username}" has joined this server`);
+  member.guild.channels.get("member-log").send(`"${member.user.username}" has joined this server`);
 });
 };
