311bf5d12d69b51c7e85974f3415e3b6beecddc8
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-06 02:18:45 +0300

    Delete guildMemberAdd.js

--- events/guildMemberAdd.js
+++
@@ -1,7 +0,0 @@
-module.exports = (client, member) => {
-
-    client.on("guildMemberAdd", (member) => {
-  console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
-  member.guild.channels.get("member-log").send(`"${member.user.username}" has joined this server`);
-});
-};
