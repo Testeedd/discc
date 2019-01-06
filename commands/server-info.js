a1e39433223411063ef9fdb00d9b5ea470b67201
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-07 02:42:08 +0300

    Update server-info.js

--- commands/server-info.js
+++ commands/server-info.js
@@ -1,15 +1,21 @@
-const Discord = require("discord.js");
+
+const Discord = require('discord.js') // подключение discord.js к файлу
 
 module.exports.run = async (bot, message, args) => {
-    let sicon = message.guild.iconURL;
-    let serverembed = new Discord.RichEmbed()
-    .setDescription("Server Information")
-    .setColor("#4169E1")
-    .setThumbnail(sicon)
-    .addField("Server Name", message.guild.name)
-    .addField("Created On", message.guild.createdAt)
-    .addField("You Joined", message.member.joinedAt)
-    .addField("Total Members", message.guild.memberCount);
-
-    message.channel.send(serverembed);
+    let verifilv = ['Отсутствует', 'Низкий', 'Средний', 'Высокий', 'Очень высокий']
+    let embed = new Discord.RichEmbed() // встроенное сообщение
+        .setAuthor(message.guild.name, message.guild.iconURL) // параметры: имя: string, картинка: string, url: string
+        .addField('Владелец', message.guild.owner, true) // параметры: название: string, значение: string, в линию?: boolean (true, false)
+        .addField('ID', message.guild.id, true)
+        .addField('Регион', message.guild.region, true)
+        .addField('Участники', `${message.guild.presences.size} в сети\n${message.guild.memberCount} всего`, true)
+        .addField('Каналы', `${message.guild.channels.filter(c => c.type == 'text').size} тестовых\n${message.guild.channels.filter(c => c.type == 'voice').size} голосовых`, true)
+        .addField('Уровень проверки', verifilv[message.guild.verificationLevel], true)
+        .addField('AFK Канал', message.guild.afkChannel.name, true)
+        .addField('Ролей', message.guild.roles.size, true)
+        .addField('Смайликов', message.guild.emojis.size, true)
+        .setFooter('Сервер создан') // параметры: текст: string, картинка: string
+        .setTimestamp(new Date(message.guild.createdTimestamp)) // конверт времени
+        .setColor(#4169E1) // цвет
+    await message.channel.send(embed) // отправка в канал
 }
