16890084d43c3c82fcc89768e60ae209cab9f03f
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-09 20:44:12 +0300

    Create somehelp.js

---
+++ commands/somehelp.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+exports.run = (client, message, args, level) => {
+
+const embed = new Discord.RichEmbed()
+  .setTitle("command: somehelp")
+  .setColor("#4169E1 ")
+  .addField("Если вы хотите видеть кто зашел и кто вышел", `Создайте канал с названием __member-log__ и все`)
+  .addField("Если вы хотите видеть репорты", `создайте канал с названием __reports__ и все`)
+  .addField("Если вы хотите видеть логи банов и киков", `создайте канал с названием __logs__ и все`)
+  .setDescription("после выполнения каждого из пунктов у вас все будет полностью работать)")
+  .setTimestamp()
+  .setFooter("|LLIAJIYH-BOT| PROJECTS All rightes reserved.", "https://i.imgur.com/fBv7xWF.jpg")
+message.channel.send({embed}).then(msg => msg.delete(30000));
+message.delete().catch(_O_o=>{});
+	} 
