678d1a2b59611b6c4bd2951f6586864d6af86db6
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-09 20:51:48 +0300

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
   let hours = Math.floor(totalSeconds / 3600);
   totalSeconds %= 3600;
   let minutes = Math.floor(totalSeconds / 60);
-  let seconds = Math.round(totalSeconds % 60);
+  let seconds = totalSeconds % 60;
 
   let uptime = `${days}дней ${hours} часов, ${minutes} минут и ${seconds} секунд`;
 
