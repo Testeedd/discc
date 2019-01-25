5177dc0bf20591816c20d80a9ea99cb3ff937c59
Testeedd <46296328+Testeedd@users.noreply.github.com>
2019-01-25 21:33:10 +0300

    Update debug.js

--- commands/debug.js
+++ commands/debug.js
@@ -3,20 +3,20 @@ const config = require("../config.json")
 //🔎 👤 ⚙ 🆔 📄 📝 ⌛ 🖍 🔊 📬 🚀
 
 
-module.exports.run = (bot, message, args, discord) => {
+module.exports.run = (client, message, _args, discord) => {
   let embed = new discord.RichEmbed()
     .setTitle(`Шалуняка бот`)
     .setTimestamp()
-    .setAuthor(`${bot.user.username} Debugger`, bot.user.avatarURL)
+    .setAuthor(`${client.user.username} Debugger`, client.user.avatarURL)
     .setColor("7289DA")
     .setDescription(`
 ⚙ **Bot Version:** ${pak.version}\n
-👤 **Bot Name:** ${bot.user.tag}\n
-🆔 **Bot ID:** ${bot.user.id}\n
+👤 **Bot Name:** ${client.user.tag}\n
+🆔 **Bot ID:** ${client.user.id}\n
 🚀 **Memory Usage:** ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\n
 🖍 **Bot Prefix:** ${config.prefix}\n
-🔎 **I know:**\n -   ${bot.guilds.size} Guilds\n -   ${bot.channels.size} Channels\n -   ${bot.users.size} Users\n
-⌛ **Uptime:** ${Math.round(bot.uptime / (1000 * 60 * 60))} hours, ${Math.round(bot.uptime / (1000 * 60)) % 60}  minutes, ${Math.round(bot.uptime / 1000) % 60} seconds.\n\n`)
+🔎 **I know:**\n -   ${client.guilds.size} Guilds\n -   ${client.channels.size} Channels\n -   ${client.users.size} Users\n
+⌛ **Uptime:** ${Math.round(client.uptime / (1000 * 60 * 60))} hours, ${Math.round(client.uptime / (1000 * 60)) % 60}  minutes, ${Math.round(client.uptime / 1000) % 60} seconds.\n\n`)
   message.channel.send({embed: embed})
 } else {
   message.channel.send("Nope!")
